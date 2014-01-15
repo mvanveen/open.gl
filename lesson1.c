f9b6752874dd4e0d8b1ab92501617b1f5c991e8c
Michael Van Veen <michael@mvanveen.net>
2014-01-14 21:57:17 -0800

    initialize gl context in window with event loop

--- lesson1.c
+++ lesson1.c
@@ -6,6 +6,20 @@ int main(int argc, char *argv[]) {
   SDL_Window* window = SDL_CreateWindow(
     "OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL
   );
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  SDL_Event windowEvent;
+
+  while (true) {
+    if (SDL_PollEvent(&windowEvent)) {
+      if (windowEvent.type == SDL_QUIT) break;
+    }
+    SDL_GL_SwapWindow(window);
+  }
+
+  // delete GL context and quit
+  SDL_GL_DeleteContext(context);
   SDL_Quit();
 
   return 0;
